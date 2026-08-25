f0404cf3930a8923b19327bad708d30af77d40d7
HanielUlises <hanielulises2003@gmail.com>
2026-08-24 22:10:24 -0600

    F-Algebra for fixpoint tree

---
+++ Abstract Algebra/f_algebra.hpp
@@ -0,0 +1,20 @@
+template<template<typename> class F>
+struct Fix
+  : F<Fix<F>>
+{
+    explicit Fix(F<Fix<F>> f)
+      : F<Fix<F>>(f)
+    {}
+};
+
+template<template<typename> class F>
+Fix<F> Fx(F<Fix<F>> f)
+{
+    return Fix<F>{f};
+}
+
+template<template<typename> class F>
+F<Fix<F>> unFix(Fix<F> f)
+{
+    return f;
+}    
